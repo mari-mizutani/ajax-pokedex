357749376445bae67c9a227165b88ddd3cd3c202
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-05-06 15:53:22 +0200

    2nd commit

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const pokeContainer = document.getElementById("pokeContainer");
 
+// ---GET DATA FROM API----
 async function callApi(){
-    const res = await fetch ("https://pokeapi.co/api/v2/");
-    const pokeApi = await res.json();
+    const generalUrl = await fetch ("https://pokeapi.co/api/v2/pokemon/");
+    const pokeApi = await generalUrl.json();
 
-    console.log(pokeApi);
+    //console.log(pokeApi.results);
+
+     // ---DOM---
+    pokeApi.results.forEach((pokemon)=>{
+        console.log(pokemon);
+    
+        document.getElementById("button").addEventListener("click",function(){
+            const profile = document.createElement("li");
+            profile.innerText = pokemon.name;
+            pokeContainer.appendChild(profile);
+        })
+    });
+
 }
 callApi();
